d1fd0205d6c6565eac99b78ca8322e1a23c02487
Eckhart Wörner <ewoerner@users.noreply.github.com>
2026-02-13 00:51:07 +0100

    Initial commit

---
+++ src/main/java/org/example/bbid/BootBuildImageDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.bbid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootBuildImageDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootBuildImageDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bbid/BootBuildImageDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bbid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBuildImageDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
